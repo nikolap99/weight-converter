f44bfb8b3b3b980de65c528cf220bc47d4f71d49
Nikola <nikolapavlov33@gmail.com>
2019-04-12 01:15:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,137 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const initialState = {
+    pounds: "",
+    grams: "",
+    kilograms: "",
+    ounces: ""
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = initialState;
+
+    handlePounds = e => {
+        const pounds = e.target.value;
+
+        this.setState({
+            pounds,
+            grams: pounds / 0.0022046,
+            kilograms: pounds / 2.2046,
+            ounces: pounds * 16
+        });
+
+        if (pounds === "") {
+            this.setState(initialState);
+        }
+    };
+
+    handleGrams = e => {
+        const grams = e.target.value;
+
+        this.setState({
+            grams,
+            pounds: grams * 0.0022046,
+            kilograms: grams / 1000,
+            ounces: grams * 0.03527
+        });
+
+        if (grams === "") {
+            this.setState(initialState);
+        }
+    };
+
+    handleKilograms = e => {
+        const kilograms = e.target.value;
+
+        this.setState({
+            kilograms,
+            pounds: kilograms * 2.2046,
+            grams: kilograms * 1000,
+            ounces: kilograms * 35.27
+        });
+
+        if (kilograms === "") {
+            this.setState(initialState);
+        }
+    };
+    handleOunces = e => {
+        const ounces = e.target.value;
+
+        this.setState({
+            ounces,
+            pounds: ounces / 16,
+            kilograms: ounces / 35.27,
+            grams: ounces / 0.03527
+        });
+
+        if (ounces === "") {
+            this.setState(initialState);
+        }
+    };
+    clearAll = () => {
+        this.setState(initialState);
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <form
+                    onSubmit={e => e.preventDefault()}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <h1 className="title">Weight Converter</h1>
+                    <TextField
+                        id="standard-number"
+                        label="Enter pounds"
+                        value={this.state.pounds}
+                        onChange={this.handlePounds}
+                        type="number"
+                        margin="normal"
+                        className="input-field"
+                    />
+                    <TextField
+                        id="standard-number"
+                        label="Enter grams"
+                        value={this.state.grams}
+                        onChange={this.handleGrams}
+                        type="number"
+                        margin="normal"
+                        className="input-field"
+                    />
+                    <TextField
+                        id="standard-number"
+                        label="Enter kilograms"
+                        value={this.state.kilograms}
+                        onChange={this.handleKilograms}
+                        type="number"
+                        margin="normal"
+                        className="input-field"
+                    />
+                    <TextField
+                        id="standard-number"
+                        label="Enter ounces"
+                        value={this.state.ounces}
+                        onChange={this.handleOunces}
+                        type="number"
+                        margin="normal"
+                        className="input-field"
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.clearAll}
+                        className="button"
+                    >
+                        Clear All
+                    </Button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
